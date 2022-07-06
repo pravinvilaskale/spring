bdeca3ce73dfd0672199aea9c2cc4984997f0693
pravinvilaskale <pravinvkale889@gmail.com>
2022-07-06 23:48:53 +0530

    Start Project

---
+++ usermodule/src/main/java/com/example/usermodule/UsermoduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermoduleApplication.class, args);
+	}
+
+}
