a6bad76915bf055c6b7e499e8c3a059fe3becac9
pravinvilaskale <pravinvkale889@gmail.com>
2022-07-08 17:47:45 +0530

    done

---
+++ usermodule/src/main/java/com/example/usermodule/UserService.java
@@ -0,0 +1,38 @@
+package com.example.usermodule;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> listAll()
+	{
+		return repo.findAll();
+	}
+	
+	public 	void save(User user) 
+	{
+		repo.save(user);
+	}
+	
+	public User get(Integer id) 
+	{
+		return repo.findById(id).get();
+	}
+	
+//	public void delete(Integer id) 
+//	{
+//		return repo.deleteById(id);
+//	}
+	
+}
--- usermodule/src/main/java/com/example/usermodule/UsermoduleApplication.java
+++ usermodule/src/main/java/com/example/usermodule/UsermoduleApplication.java
@@ -2,12 +2,15 @@ package com.example.usermodule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan("com.example.usermodule.UserController")
 public class UsermoduleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsermoduleApplication.class, args);
 	}
-
+	
 }
